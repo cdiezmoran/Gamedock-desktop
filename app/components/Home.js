54a01ad9c6d0282b8648c3e0ae96a0593a36fe50
Carlos Diez <cdiezmoran@gmail.com>
2016-11-10 23:57:18 -0800

    Started desktop capturer

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,40 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { desktopCapturer } from 'electron';
 import styles from './Home.css';
 
 
 export default class Home extends Component {
+  captureDesktop() {
+    // Change state to capturing
+
+    // Get sources and select which one we want using props
+    desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
+      for(let source of sources) {
+        // TODO: Replace gamedock with game name from props
+        if(source === 'Gamedock') {
+          navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: {
+              mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id,
+                minWidth: 1280,
+                maxWidth: 1280,
+                minHeight: 720,
+                maxHeight: 720
+              }
+            }
+          }).then((stream) => {
+
+          }).catch((error) => {
+
+          });
+        }
+      }
+    }
+  },
   render() {
     return (
       <div>
