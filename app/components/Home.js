2d41930c12c322acadce199035061bd9da322781
Carlos Diez <cdiezmoran@gmail.com>
2016-11-11 11:54:03 -0800

    Streaming desktop capture video working

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,20 +2,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { desktopCapturer } from 'electron';
+import $ from 'jquery';
 import styles from './Home.css';
 
-
-export default class Home extends Component {
-  captureDesktop() {
+class Home extends Component {
+  handleCapture() {
     // Change state to capturing
 
     // Get sources and select which one we want using props
     desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
       for(let source of sources) {
         // TODO: Replace gamedock with game name from props
-        if(source === 'Gamedock') {
+        console.log(source.name)
+        if(source.name === "Slack - Make School Product Academy '18") {
+          console.log("Getting media for " + source.name)
           navigator.mediaDevices.getUserMedia({
-            audio: true,
+            audio: false,
             video: {
               mandatory: {
                 chromeMediaSource: 'desktop',
@@ -27,21 +29,33 @@ export default class Home extends Component {
               }
             }
           }).then((stream) => {
-
+            // TODO: Handle stream
+            console.log("Got stream")
+            $('#video').attr("src", URL.createObjectURL(stream))
           }).catch((error) => {
-
+            console.log(error);
           });
         }
       }
-    }
-  },
+    });
+  }
+
+  handleStop() {
+    // TODO: Stop the capture
+  }
+
   render() {
     return (
       <div>
         <div className={styles.container}>
-          <h2>Gamedock Home</h2>
+          <h2> Home</h2>
+          <video width="640" height="360" id="video" autoPlay/>
+          <button onClick={this.handleCapture}>Capture</button>
+          <button onClick={this.handleStop}>Stop</button>
         </div>
       </div>
     );
   }
 }
+
+export default Home
